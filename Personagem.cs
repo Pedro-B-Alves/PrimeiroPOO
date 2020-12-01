3616a2534c9f29362509519e7c79e65799bb739f
Pedro Alves <pedro22carro@gmail.com>
2020-12-01 16:25:22 -0300

    Começo da luta

---
+++ Personagem.cs
@@ -0,0 +1,17 @@
+namespace PrimeiroPOO
+{
+    public class Personagem
+    {
+        public string nome;
+        public int idade;
+        public string armadura;
+
+        public string Atacar(){
+            return "Ataque de fogo";
+        }
+
+        public string Defender(){
+            return "Não  foi dessa vez!";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PrimeiroPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personagem objetoPersonagem1 = new Personagem();
+
+            objetoPersonagem1.nome = "Homem De Ferro";
+            objetoPersonagem1.idade = 53;
+            objetoPersonagem1.armadura = "Destruidora";
+
+            Personagem objetoPersonagem2 = new Personagem();
+
+            objetoPersonagem2.nome = "Capitão America";
+            objetoPersonagem2.idade = 39;
+            objetoPersonagem2.armadura = "Indestrutível";
+
+            Console.WriteLine($"Personagem1 = {objetoPersonagem1.nome} - {objetoPersonagem1.Atacar()}");
+            Console.WriteLine($"Personagem2 = {objetoPersonagem2.nome} - {objetoPersonagem2.Defender()}");
+        }
+    }
+}
